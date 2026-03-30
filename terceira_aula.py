aed318afa34f13e69e9165463f44b0a63932ee8f
DaviBaruch <davibaruchg@gmail.com>
2026-03-30 20:39:28 -0300

    criado uma file: correcao_chechpoint.py e organizar o README

--- terceira_aula.py
+++ terceira_aula.py
@@ -1,19 +1,19 @@
 #If condicionais
 from segunda_aula import salario
 
-#exercicios
+# exercicios
 
-#1. Escreva um programa que pergunte a velocidade do carro de um usuário. Caso ultrapasse 80km/h, exiba uma mensagem dizendo que o usuário foi multado. Nesse caso, exiba o valor da multa, cobrando R$5 por km acima de 80km/h
+# 1. Escreva um programa que pergunte a velocidade do carro de um usuário. Caso ultrapasse 80km/h, exiba uma mensagem dizendo que o usuário foi multado. Nesse caso, exiba o valor da multa, cobrando R$5 por km acima de 80km/h
 
-# velocidade = float(input("Qual foi sua velocidade maxima? "))
-#
-# if velocidade > 80:
-#      multa = (velocidade - 80) * 5
-#      print("vc recebeu uma multa")
-#      print(f"valor da multa R${multa:.2f}.",)
-#
-# else:
-#     print("Velocidade dentro do limite")
+velocidade = float(input("Qual foi sua velocidade maxima? "))
+
+if velocidade > 80:
+     multa = (velocidade - 80) * 5
+     print("vc recebeu uma multa")
+     print(f"valor da multa R${multa:.2f}.",)
+
+else:
+    print("Velocidade dentro do limite")
 
 
 #2. Escreva um programa que leia três números e que imprima o maior e o menor
@@ -54,4 +54,19 @@ if distancia <= 200 :
 else:
     preco = distancia * 0.45
 print(f"O preço da passagem R${preco:.2f}.")
+
+#5. Escreva um programa que leia dois números e que pergunte qual operação você deseja realizar. Você deve poder calcular soma, subtração, multiplicação e divisão. Exiba o resultado da operação solicitada.
+
+print("Calculadora. Escolha os números que você quer calcular")
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo: "))
+
+if operacao == "+":
+    print(a + b)
+elif operacao == "-":
+    print(a - b)
+elif operacao == "*":
+    print(a * b)
+elif operacao == "/":
+    print(a / b)
 
