297bba8c2ef31112f5df3bf1d50cb02d5019ee66
Davi Baruch <davibaruchg@gmail.com>
2026-05-11 19:50:38 -0300

    fizemos media de notas usando listas e while

--- quinta_aula.py
+++ quinta_aula.py
@@ -5,3 +5,12 @@ print(compras_super)
 
 #media de notas
 
+notas_check = [5, 8, 10, 7]
+soma = 0
+x = 0
+while x < 4:
+    soma = soma + notas_check[x]
+    x = x + 1
+
+    print(f"Média = {soma/4}")
+    
