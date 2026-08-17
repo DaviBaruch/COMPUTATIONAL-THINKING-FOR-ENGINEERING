6196ffdb2454766ab1aebccc0c3b078f4c3092b2
Davi Baruch <davibaruchg@gmail.com>
2026-08-17 20:24:13 -0300

    criando uma lista de dicionários de alunos com as informações: Nome, RM, Nota e Turma

--- setima_aula.py
+++ setima_aula.py
@@ -5,3 +5,25 @@ idade_familia = [
     [22, 46, 40, 17]]
 
 print(idade_familia[1][0])
+
+#Usando em um exemplo
+#Davi
+#RM572086   
+#9.0
+#1ECR
+lista_alunos = [
+    {"Nome": "Davi", 
+     "RM": "572086", 
+     "Nota": 9.0, 
+     "Turma": "1ECR"},
+
+   {"Nome": "Gabi",
+    "RM": "572089", 
+    "Nota": 8.0, 
+    "Turma": "1ECR"},
+]
+
+
+print(lista_alunos[0]["Nome"], lista_alunos [0]["Nota"])
+for aluno in lista_alunos:
+    print(aluno["Nome"], aluno["Nota"] )
